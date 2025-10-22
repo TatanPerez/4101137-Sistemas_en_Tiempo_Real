a8639ad596f6212c36e36902c300ff33e07dd60c
Tatan Perez <wiperezc@unal.edu.co>
2025-10-22 15:22:16 -0500

    Split into source and header files

---
+++ Tasks/ADC_EXAMPLE/main/oneshot_read.h
@@ -0,0 +1,58 @@
+#ifndef ONESHOT_READ_H
+#define ONESHOT_READ_H
+
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "driver/gpio.h"
+#include <stdbool.h>
+
+/*---------------------------------------------------------------
+        ADC General Macros
+---------------------------------------------------------------*/
+//ADC1 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_4
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_5
+#else
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_2
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_1
+#endif
+
+#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
+/**
+ * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
+ * Search for errata on espressif website for more details.
+ */
+#define EXAMPLE_USE_ADC2            1
+#endif
+
+#if EXAMPLE_USE_ADC2
+//ADC2 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#else
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#endif
+#endif  //#if EXAMPLE_USE_ADC2
+
+#define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
+#define BUTTON_GPIO GPIO_NUM_9  // GPIO del botón para habilitar/deshabilitar impresión
+/*---------------------------------------------------------------
+        ADC Example Function Declarations   
+---------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+void example_adc_calibration_deinit(adc_cali_handle_t handle);
+
+// Nueva función que contiene toda la lógica
+void oneshot_read_start(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ONESHOT_READ_H
